54f4ad35e069254e431d548d3841c2eae4363e68
vaibhavmaloo03 <vmaloo@ucsd.edu>
2024-04-30 22:39:56 -0700

    just checking

--- expose/javascript/part2-question16.js
+++ expose/javascript/part2-question16.js
@@ -8,6 +8,8 @@ let statistics = {
     };
     for (let prop in statistics) {
       if (prop[0] === 'r' || statistics[prop] % 2 !== 0) {
+        console.log();
         console.log(statistics[prop]);
       }
+
     }
